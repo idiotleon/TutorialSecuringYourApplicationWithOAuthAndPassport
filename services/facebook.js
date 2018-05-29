ed4d88a41a9d590d70cc4e260bd2fbeab2f2db4a
Leon Lu <yanglu@microsoft.com>
2018-05-29 16:33:52 -0700

    demo: The Graph API and Our Facebook service
    Module 4.3

---
+++ services/facebook.js
@@ -0,0 +1,31 @@
+var OAuth = require('OAuth').OAuth2;
+
+var Facebook = function (facebookKey, facebookSecret) {
+
+    var key = facebookKey;
+    var secret = facebookSecret;
+
+    var oauth = new OAuth(
+        key, secret,
+        'https://graph.facebook.com',
+        null,
+        'oauth2/token',
+        null
+    );
+
+    var getImage = function (userKey, done) {
+        oauth.get(
+            'https://graph.facebook.com/v3.0/me/picture?redirect=false&type=large',
+            userKey,
+            function (err, results, response) {
+                results = JSON.parse(results);
+                done(results.data);
+            });
+    };
+
+    return {
+        getImage: getImage
+    }
+}
+
+module.exports = Facebook;
